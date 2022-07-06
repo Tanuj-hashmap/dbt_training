3b8aba4cd9d885f1cacdc53ca61a0365db387650
tanuj.bhatt@hashmapinc.com <tanuj.bhatt@hashmapinc.com>
2022-07-06 06:53:43 +0000

    test

--- models/stage/test.sql
+++ models/stage/test.sql
@@ -4,6 +4,6 @@
     abcd='wsdwsf'
   ) }}
 -- select * from {{database}}."RAW"."ORDERS"
-
--- select {{abcd}}
-select * from {{ source('RAW', 'ORDERS') }}
+select '1' as numbr
+-- select current_date() as d, {{abcd}} as dummy
+-- select * from {{ source('RAW', 'ORDERS') }}
